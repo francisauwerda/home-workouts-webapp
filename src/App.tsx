cb1dbbc332aafafa0a6debe2a14bc593b141e906
Francis Auwerda <fauwerda@gmail.com>
2020-05-13 08:25:56 +0200

    Add apollo provider and client and fetch workouts to screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import HomePage from './components/pages/HomePage';
 import WorkoutPage from './components/pages/WorkoutPage';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/workout">
-          <WorkoutPage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route path="/workout">
+            <WorkoutPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from 'apollo-boost';
 
-const HomePage = () => (<div>Home page</div>)
+const WORKOUTS = gql`
+  {
+    workouts {
+      id
+      title
+      exercises {
+        id
+        title
+        imageUrl
+        order
+      }
+    }
+  }
+`;
+
+type Workout = {
+  id: string,
+  title: string,
+}
+
+const HomePage = () => {
+  const { loading, error, data } = useQuery(WORKOUTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  
+  return data.workouts.map((workout: Workout) => {
+    return <p>{workout.title}</p>
+  })
+}
 
 export default HomePage;
