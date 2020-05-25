46c2b975400da7cc5f5be7324e9dddbde2b355a8
Francis Auwerda <fauwerda@gmail.com>
2020-05-25 07:26:30 +0200

    Add styles to the image

--- src/components/pages/WorkoutDetailsPage/styled.ts
+++ src/components/pages/WorkoutDetailsPage/styled.ts
@@ -7,7 +7,6 @@ export const H3 = styled.h3`
 
 export const ExerciseWrapper = styled.div`
   display: flex;
-  /* background-color: grey; */
   margin-top: 60px;
 `;
 
@@ -25,10 +24,14 @@ export const ExerciseTitle = styled(H3)`
 export const ExerciseImageContainer = styled.div`
   flex: 1;
   width: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const ExerciseImage = styled.img`
   max-width: 100%;
+  max-height: 600px;
 `;
 
 export const TimerWrapper = styled.div`
